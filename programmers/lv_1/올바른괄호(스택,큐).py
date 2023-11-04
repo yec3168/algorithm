579940f39e4a7b7e2d0e37bda772637f7400e3b7
yec3168 <yec3168@naver.com>
2023-11-04 17:43:52 +0900

    올바른 커밋

--- programmers/lv_1/올바른괄호(스택,큐).py
+++ programmers/lv_1/올바른괄호(스택,큐).py
@@ -17,30 +17,5 @@ def solution(s):
 
 
 
-# def solution(s):
-#     queue =[]
-#     result = False
-#     for i in s:
-#         queue.append(i)
-
-#     # 첫 번째가 ")"이거나 큐의 수가 홀수면 False
-#     if queue[0]== ")" or len(queue) % 2 != 0 or len(queue) == 0:
-#         return result
-
-#     count =[0, 0]
-
-#     for i in queue:
-#         if i == "(":
-#             count[0] = count[0] + 1
-#         else:
-#             count[1] = count[1] + 1
-        
-#     if count[0] == count[1]:
-#         result = True
-    
-#     return result
-
-
-
 str ="()()"
 print(solution(str))
