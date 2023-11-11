fa5f5ffaf1de239ed558b725237404e916f4e828
yec3168 <yec3168@naver.com>
2023-11-12 01:48:50 +0900

    연속 부분 수열 합의 개수

--- programmers/lv_2/연속 부분 수열 합의 개수.py
+++ programmers/lv_2/연속 부분 수열 합의 개수.py
@@ -17,6 +17,24 @@ def solution(elements):
     result.pop()
     return len(result)
 
+# 다른사람풀이
+def solution(elements):
+    result = set()
+
+    i = 0 
+    while i < len(elements):
+        i += 1
+        for j in range(len(elements)):
+            if i + j <= len(elements):
+                # 0 : 0+1   0: 0+2  0 : 0+3     0 : 0+4
+                # 1 : 1+1
+                #print((elements[j : j+i]))
+                result.add(sum(elements[j : j+i])) 
+            else:
+                result.add(sum(elements[j:])+sum(elements[:i+j-len(elements)]))
+                
+
+    return len(result)
 
 
 
