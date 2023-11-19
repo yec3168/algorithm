6df9c568b3a2addf08e53e258b525e0197aa7280
yec3168 <yec3168@naver.com>
2023-11-19 20:28:05 +0900

    행렬의 곱셉

--- programmers/lv_2/행렬의 곱셉.py
+++ programmers/lv_2/행렬의 곱셉.py
@@ -1,3 +1,7 @@
+import numpy as np
+import json
 def solution(arr1, arr2):
-    answer = [[]]
-    return answer
+    arr3=np.array(arr1)
+    arr4=np.array(arr2)
+    result=np.dot(arr3,arr4)
+    return result.tolist()
