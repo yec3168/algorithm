0d087fd3b33c853ce8a77b2cf7f6f21904b97bc5
yec3168 <yec3168@naver.com>
2023-11-29 00:25:35 +0900

    실패율[카카오]

--- programmers/lv_1/실패율[카카오].py
+++ programmers/lv_1/실패율[카카오].py
@@ -3,16 +3,27 @@
 def solution(N, stages):
     stages.sort()
     dic = {}
+    temp = []
+    count = 1
     
     for i in range(1,N+1):
         dic[str(i)] = stages.count(i)
-    dic[str(N)]  += stages.count(N+1)
 
-    
+    N = len(stages)
+    while True:
+        if count > len(dic):
+            break
+        if dic[str(count)] == 0:
+            temp.append( [count, 0])
+        else:
+            temp.append([count, dic[str(count)]/N])
+        N -=dic[str(count)]
+        count +=1
+    temp = sorted(temp, key = lambda x : x[1], reverse=True)
+    result =[]
+    for i in temp:
+        result.append(i[0])
 
-
-    
-    
-
-
-print(solution(5,	[2, 2, 2, 6, 2, 4, 3, 3]))
+    return result
+print(solution(5,	[2, 1, 2, 6, 2, 4, 3, 3]))
+print(solution(4,	[4,4,4,4,4]))
