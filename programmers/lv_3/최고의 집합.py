a6187568e57e0a193e0facaaca8241e903a2f9d4
yec3168 <yec3168@naver.com>
2023-11-30 14:58:22 +0900

    최고의 집합

---
+++ programmers/lv_3/최고의 집합.py
@@ -0,0 +1,19 @@
+#1. 각 원소의 합이 S가 되는 수의 집합
+#2. 위 조건을 만족하면서 각 원소의 곱 이 최대가 되는 집합
+def solution(n, s):
+    if n > s:
+        return [-1]
+    
+    result =[ (s//n)  for _ in range(n) ]
+
+    temp = s - sum(result)
+
+    for i in range(temp):
+        result[-1-i] += 1
+
+    return result   
+
+    
+    
+
+print(solution(2, 9))
