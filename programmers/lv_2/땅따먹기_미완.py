a13b17728e09849e054448e7d51c8f0392661f65
yec3168 <yec3168@naver.com>
2023-12-04 15:48:28 +0900

    땅따먹기_미완

--- programmers/lv_2/땅따먹기_미완.py
+++ programmers/lv_2/땅따먹기_미완.py
@@ -2,6 +2,8 @@
 #같은 열을 연속해서 밟을 수 없는 특수 규칙이 있습니다.
 
 
+# 횟수가 100,000 이상이기 떄문에 재귀가 아닌 반복으로 풀 돼,
+#최대를 구하는 것이니 DP로 풀어야할 듯 : 점화식 구하기
 # 나오긴 하는데 재귀 함수횟수 초과 시간 초과
 import sys
 sys.setrecursionlimit(10^10)
@@ -14,7 +16,7 @@ def dfs(land ,cur_position, cur_sum, cur_depth):
         for i in range(1, 4):
             result = max(result, dfs(land,(cur_position+i)%4,cur_sum+land[cur_depth][(cur_position+i)%4],cur_depth+1))
     return result
-def solution2(land):
+def solution(land):
     
     return dfs(land, 0, 0, 0)
 
